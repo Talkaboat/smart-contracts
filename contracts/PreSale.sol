6c4c5e4be2551bd2627d5f4fbc9eddf02a8b9849
Fabi <fabian.reichenberg@outlook.de>
2021-11-11 08:25:01 +0100

    added vesting to presale contract

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -12,21 +12,32 @@ contract PreSale is Ownable {
     using Address for address;
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
+    
+    
+    
     /* =====================================================================================================================
                                                         Variables
     ===================================================================================================================== */
-    IERC20 public rewardToken;
-    IERC20 public paymentToken;
-    uint256 public pricePerToken;
-    uint256 public limit;
+    IERC20 public rewardToken; //Aboat Token
+    IERC20 public paymentToken; //BNB
+    uint256 public pricePerToken;   //How much BNB per Aboat Token
+    uint256 public limit;   //how much can each investors spend at maximum
     uint256 public softcap; //minimum required sell (how many tokens should be sold)
-    uint256 public soldTokens;
-    uint256 public saleEnded = 0;
-    uint256 public afterDays;
+    uint256 public soldTokens; //how many token are currently sold
+    uint256 public saleEnded = 0; //block when the sale ended (0 = still ongoing)
+    uint256 public afterDays; //after how many days can investors make their initial claim
+    uint256 public period = 1; //How many days are between each claim 
+    uint256 public initialClaimPercentage = 400; //How much can investors claim directly after sale ended (default: 40%)
+    uint256 public percentagePerPeriod = 50; //How much can investors claim per period after the initial claim (default: 5% -> 1 year vesting)
+    
+    
+    bool public requireWhitelist = true;    //flag to determine whether buyers have to be whitelisted or not
     
     mapping(address => bool) public whitelisted;
-    mapping(address => uint256) public bought;
-    mapping(address => bool) public claimed;
+    mapping(address => uint256) public bought;  //tracks who bought how many aboat token
+    mapping(address => uint256) public claimed; //tracks who claimed how much percentage of his tokens
+    mapping(address => uint256) public claimedTokens; //tracks who claimed how many aboat token
+    mapping(address => address) public lastClaimAddress; //In case we have to swap the token contract
 
     /* =====================================================================================================================
                                                         Events
@@ -34,7 +45,10 @@ contract PreSale is Ownable {
     event SaleEnded(uint256 indexed claimDate);
     event Claimed(address indexed owner, uint256 indexed amount);
     event Bought(address indexed buyer, uint256 indexed amount);
+    event ChangeRewardToken(address indexed newToken);
+    
     constructor(IERC20 _rewardToken, IERC20 _paymentToken, uint256 _limit, uint256 _softcap, uint256 _price) {
+        require(_price > 0, "ABOAT::error: Price has to be higher than zero");
         rewardToken = _rewardToken;
         paymentToken = _paymentToken;
         limit = _limit;
@@ -67,16 +81,22 @@ contract PreSale is Ownable {
                 TransferHelper.safeTransferETH(msg.sender,  address(this).balance);
             }
         }
-        emit SaleEnded(saleEnded.add(afterDays.mul(1 minutes)));
+        emit SaleEnded(saleEnded.add(afterDays.mul(1 days)));
+    }
+    
+    function disableWhitelist() public onlyOwner {
+        require(requireWhitelist, "ABOAT:disableWhitelist: Whitelist is already disabled");
+        requireWhitelist = false;
     }
     
     //Will only be required if the security audit displays errors that have to be fixed
     //which would mean a new contract has to be deployed.
-    //Early investors should still be able to get the right coin
+    //With this function we can ensure that early investors will still be able to get the right coin before release
     function updateRewardToken(IERC20 _newRewardToken) public onlyOwner {
         require(_newRewardToken != rewardToken, "ABOAT::updateRewardToken: New reward should be different from current.");
         require(_newRewardToken.balanceOf(address(this)) == rewardToken.balanceOf(address(this)), "ABOAT::updateRewardToken: The contract should contain atleast the same amount of tokens as from the current rewardToken");
         rewardToken = _newRewardToken;
+        emit ChangeRewardToken(address(rewardToken));
     }
     
     
@@ -98,6 +118,11 @@ contract PreSale is Ownable {
         }
     }
     
+    function getCurrentPercentage() public view returns (uint256) {
+        uint256 percentage = saleEnded > 0 && block.timestamp > saleEnded.add(afterDays * 1 days) ? block.timestamp.sub(saleEnded.add(afterDays * 1 days)).div(period * 1 days).mul(50).add(400) : 0;
+        return percentage > 1000 ? 1000 : percentage;
+    }
+    
     
     /* =====================================================================================================================
                                                         Investors
@@ -105,7 +130,8 @@ contract PreSale is Ownable {
     
     function buy(uint256 amount) public payable {
         require(saleEnded == 0, "ABOAT::buy: Sale already ended!");
-        require(whitelisted[msg.sender], "ABOAT::buy: You're not whitelisted for this sale!");
+        require(amount >= 1 ether / 20, "ABOAT::buy: minimum buy is 0.05 BNB");
+        require(whitelisted[msg.sender] || !requireWhitelist, "ABOAT::buy: You're not whitelisted for this sale!");
         bool isEthToken = address(paymentToken) == address(0);
         require(!isEthToken || msg.value == amount, "ABOAT::buy: Sent value doesn't meet the given amount");
         require(bought[msg.sender].add(amount) <= limit, "ABOAT::buy: Amount would exceed the maximum allowed limit");
@@ -116,6 +142,7 @@ contract PreSale is Ownable {
         }
         bought[msg.sender] = bought[msg.sender].add(amount);
         soldTokens = soldTokens.add(amountBought);
+        lastClaimAddress[msg.sender] = address(paymentToken);
         emit Bought(msg.sender, amount);
     }
     
@@ -123,23 +150,34 @@ contract PreSale is Ownable {
     //otherwise it returns the paid paymentToken
     function claim() public {
         require(saleEnded != 0, "ABOAT::claim: Sale is not over yet!");
-        require(!claimed[msg.sender], "ABOAT::claim: Already claimed tokens");
-        require(block.timestamp >= saleEnded.add((afterDays.mul(1 minutes))), "ABOAT::claim: Claim is not available yet.");
-        claimed[msg.sender] = true;
-        uint256 amount = bought[msg.sender].mul(1e18).div(pricePerToken);
+        require(block.timestamp >= saleEnded.add((afterDays.mul(1 days))), "ABOAT::claim: Claim is not available yet.");
+        uint256 currentPercentage = getCurrentPercentage();
+        require(currentPercentage > 0, "ABOAT::claim: The percentage of token you can claim is currently zero. Please try again later");
+        if(lastClaimAddress[msg.sender] != address(rewardToken)) {
+            lastClaimAddress[msg.sender] = address(rewardToken);
+            claimed[msg.sender] = 0;
+            claimedTokens[msg.sender] = 0;
+        }
+        require(claimed[msg.sender] < currentPercentage, "ABOAT::claim: Already claimed your currently eligible tokens");
         if(softcap <= soldTokens) {
+            claimed[msg.sender] = currentPercentage;
+            uint256 amount = bought[msg.sender].mul(currentPercentage).div(1000).mul(1e18).div(pricePerToken);
+            claimedTokens[msg.sender] = amount;
             if(address(rewardToken) != address(0)) {
                 TransferHelper.safeTransfer(address(rewardToken), msg.sender, amount);
             } else {
                 TransferHelper.safeTransferETH(msg.sender, amount);
             }
+            emit Claimed(msg.sender, amount);
         } else {
+            claimed[msg.sender] = 1000; //
             if(address(paymentToken) != address(0)) {
                 TransferHelper.safeTransfer(address(paymentToken), msg.sender, bought[msg.sender]);
             } else {
                 TransferHelper.safeTransferETH(msg.sender, bought[msg.sender]);
             }
+            emit Claimed(msg.sender, bought[msg.sender]);
         }
-        emit Claimed(msg.sender, amount);
+
     }
 }
